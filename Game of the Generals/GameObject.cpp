eed66c9d6642af7dc35358ee53f5a5914b3ceffc
Aldrey Gaurana <gauranadrei@yahoo.com>
2021-04-03 16:10:48 +0800

    updated game object

--- Game of the Generals/GameObject.cpp
+++ Game of the Generals/GameObject.cpp
@@ -1,1 +1,72 @@
 #include "GameObject.h"
+#include <iostream>
+
+
+GameObject::GameObject(string n) 
+{
+	name = n;
+}
+
+GameObject::~GameObject() 
+{
+	if (sprite != NULL) 
+	{
+		delete sprite;
+	}
+}
+
+string GameObject::getName() 
+{
+	return name;
+}
+
+void GameObject::draw(RenderWindow* window, RenderStates renderStates) 
+{
+	//apply the transform to its children
+	renderStates.transform = renderStates.transform * transformable.getTransform();
+
+	window->draw(*sprite, renderStates);
+
+	cout << "Drawing " + getName() + "\n";
+
+	//draw its children
+	for (int i = 0; i < childList.size(); i++) 
+	{
+		GameObject* child = childList[i];
+		child->draw(window, renderStates);
+	}
+}
+
+void GameObject::update(Time deltaTime) 
+{
+	// Add Implementation here
+
+}
+
+void GameObject::attachChild(GameObject* child) 
+{
+	childList.push_back(child);
+	child->initialize(); //initialize when attaching to child
+}
+
+void GameObject::detachChild(GameObject* child)
+{
+	int index = -1;
+	for (int i = 0; i < childList.size(); i++) 
+	{
+		if (childList[i] == child) 
+		{
+			index = i;
+		}
+	}
+
+	if (index != -1) 
+	{
+		childList.erase(childList.begin() + index);
+	}
+}
+
+void GameObject::setPosition(float x, float y)
+{
+	transformable.setPosition(x, y);
+}
--- Game of the Generals/GameObject.h
+++ Game of the Generals/GameObject.h
@@ -1,5 +1,29 @@
 #pragma once
+#include <string>
+#include <SFML/Graphics.hpp>
+
+using namespace std;
+using namespace sf;
+
 class GameObject
 {
+public:
+	GameObject(string name);
+	virtual ~GameObject();
+	virtual void initialize() = 0;
+	virtual void processInput(Event event) = 0;
+	virtual void update(Time deltaTime);
+	void draw(RenderWindow* window, RenderStates renderStates);
+	string getName();
+	void attachChild(GameObject* child);
+	void detachChild(GameObject* child);
+	void setPosition(float x, float y);
+
+private:
+	Transformable transformable;
+	string name;
+	Sprite* sprite;
+	Texture* texture;
+	vector<GameObject*> childList;
 };
 
